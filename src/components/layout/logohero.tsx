2690439e19a2d4b0638564e58afd677c327ce0ba
Juani Suarez <juanisua3@gmail.com>
2025-10-27 19:45:24 +0000

    le quiero dar el 100% del tamaño a la imagen logohero

--- src/components/layout/logohero.tsx
+++ src/components/layout/logohero.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-export function Logo() {
+export function LogoHero() {
   return (
     <Link href="/" className="flex items-center space-x-2" aria-label="Volver a la página de inicio de ProyectoVia">
-      <Image src="/logo.png" alt="ProyectoVia Logo" width={400} height={107} className="w-full h-full" />
+      <Image src="/logo.png" alt="ProyectoVia Logo" width={100%} height={100%} className="w-auto h-8" />
     </Link>
   );
 }
